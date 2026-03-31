cd7ceb2c64f1f7c2a738b05675138f2041d0fc15
Giriraj Sivaraj <girirajs@infinite.com>
2026-03-31 20:10:06 +0530

    ThirdAssignment

---
+++ Csharp/Assignments/ThirdAssignmentCSharp/ThirdAssignmentCSharp/Accounts.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using ThirdAssignmentCSharp;
+
+namespace ThirdAssignmentCSharp
+{
+
+    internal class Accounts
+    {
+        protected static int counter = 1000; 
+
+        public string Account_no;
+        public string Customer_name, Account_type, Transaction_type;
+        public float amount, balance;
+
+        public Accounts(string name, string accType, string transType, float amt, float bal)
+        {
+            counter++;
+            Account_no = "ACC" + counter; 
+
+            Customer_name = name;
+            Account_type = accType;
+            Transaction_type = transType;
+            amount = amt;
+            balance = bal;
+        }
+        public void Credit(float amt)
+        {
+            balance += amt;
+        }
+
+        public void Debit(float amt)
+        {
+            balance -= amt;
+        }
+        public void ProcessTransaction()
+        {
+            if (Transaction_type == "D")
+                Credit(amount);
+            else if (Transaction_type == "W")
+                Debit(amount);
+            else
+                Console.WriteLine("Invalid Transaction");
+        }
+
+        
+        public void ShowData()
+        {
+            Console.WriteLine("\nAccount No: " + Account_no);
+            Console.WriteLine("Customer Name: " + Customer_name);
+            Console.WriteLine("Account Type: " + Account_type);
+            Console.WriteLine("Transaction: " + Transaction_type);
+            Console.WriteLine("Amount: " + amount);
+            Console.WriteLine("Balance: " + balance);
+        }
+    }
+    class SavingsAccount : Accounts
+    {
+        public SavingsAccount(string name, string transType, float amt, float bal)
+            : base(name, "Savings", transType, amt, bal)
+        {
+        }
+    }
+    class CurrentAccount : Accounts
+    {
+        public CurrentAccount(string name, string transType, float amt, float bal)
+            : base(name, "Current", transType, amt, bal)
+        {
+        }
+    }
+
+    
+}
---
+++ Csharp/Assignments/ThirdAssignmentCSharp/ThirdAssignmentCSharp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdAssignmentCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SavingsAccount s = new SavingsAccount("Jeevan", "D", 5000, 10000);
+            s.ProcessTransaction();
+            s.ShowData();
+
+            CurrentAccount c = new CurrentAccount("Arun", "W", 2000, 15000);
+            c.ProcessTransaction();
+            c.ShowData();
+
+            Result s1= new Result(101, "Jeevan", "BCA", 4, "CS");
+
+            s1.ShowData();      // base class
+            s1.GetMarks();      // derived
+            s1.DisplayResult();
+
+
+
+            SaleDetails obj = new SaleDetails(101, 5001, 100.5, 2, "31-03-2026");
+
+            obj.Sales();  
+
+            SaleDetails.ShowData(obj);
+        }
+    }
+}
