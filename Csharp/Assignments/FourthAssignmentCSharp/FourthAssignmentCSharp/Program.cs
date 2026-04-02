8183e1a8616468e180df8acaca9feaa47d4542e4
Giriraj Sivaraj <girirajs@infinite.com>
2026-04-02 23:40:16 +0530

    FourthAssignmentDone

---
+++ Csharp/Assignments/FourthAssignmentCSharp/FourthAssignmentCSharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourthAssignmentCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FirstQuestion q1 = new FirstQuestion();
+            SecondQuestion q2 = new SecondQuestion();
+            ThirdQuestion q3 = new ThirdQuestion();
+
+            Console.WriteLine("---- Question 1 ----");
+            q1.RemoveChar();
+
+            Console.WriteLine("\n---- Question 2 ----");
+            q2.SwapChars();
+
+            Console.WriteLine("\n---- Question 3 ----");
+            q3.SortStack();
+        }
+    }
+}
---
+++ Csharp/Assignments/FourthAssignmentCSharp/FourthAssignmentCSharp/ThirdQuestion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourthAssignmentCSharp
+{
+    internal class ThirdQuestion
+    {
+        public void SortStack()
+        {
+            Stack<int> stack = new Stack<int>();
+
+            Console.Write("Enter number of elements: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter elements:");
+            for (int i = 0; i < n; i++)
+            {
+                stack.Push(int.Parse(Console.ReadLine()));
+            }
+
+            int[] arr = stack.ToArray();
+            Array.Sort(arr);
+            Array.Reverse(arr);
+
+            Stack<int> sortedStack = new Stack<int>();
+
+            foreach (int item in arr)
+            {
+                sortedStack.Push(item);
+            }
+
+            Console.WriteLine("Sorted Stack (Descending):");
+            foreach (int item in sortedStack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
