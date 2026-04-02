e8567468a5c99c5af0ba97a764d705674051a3ec
Giriraj Sivaraj <girirajs@infinite.com>
2026-04-02 11:49:41 +0530

    FirstAssessemnt Done

---
+++ Csharp/Assessment/FirstAssessmentCSharp/FirstAssessmentCSharp/Employee.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstAssessmentCSharp
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public double Salary { get; set; }
+
+        public static List<Employee> employees = new List<Employee>();
+
+
+
+        public static void AddEmployee()
+        {
+            Employee emp = new Employee();
+
+            Console.WriteLine("Enter the Id:");
+            emp.Id = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Name:");
+            emp.Name = Console.ReadLine();
+
+            Console.WriteLine("Enter the Department:");
+            emp.Department = Console.ReadLine();
+
+            Console.WriteLine("Enter the Salary:");
+            emp.Salary = Convert.ToDouble(Console.ReadLine());
+
+            employees.Add(emp);
+
+            Console.WriteLine("Employee Added Successfully!");
+            Employee.ViewEmployees();
+        }
+
+        public static void ViewEmployees()
+        {
+            if (employees.Count == 0)
+            {
+                Console.WriteLine("No employee Founded");
+                return;
+            }
+            foreach (var emp in employees)
+            {
+                Console.WriteLine($"ID: {emp.Id}, Name: {emp.Name}, Dept: {emp.Department}, Salary: {emp.Salary}");
+            }
+        }
+        public static void SearchEmployee()
+        {
+            Console.Write("Enter ID to search: ");
+
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            Employee found = null;
+
+            foreach (Employee emp in employees)
+            {
+                if (emp.Id == id)
+                {
+                    found = emp;
+                    break;
+                }
+            }
+
+            if (found != null)
+                Console.WriteLine($"Found: {found.Name}, {found.Department}, {found.Salary}");
+            else
+                Console.WriteLine("Employee not found.");
+        }
+        public static void UpdateEmployee()
+        {
+            Console.Write("Enter ID to update: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            Employee found = null;
+
+            foreach (Employee emp in employees)
+            {
+                if (emp.Id == id)
+                {
+                    found = emp;
+                    break;
+                }
+            }
+
+            if (found != null)
+            {
+                Console.WriteLine("\nWhat do you want to update?");
+                Console.WriteLine("1. Name");
+                Console.WriteLine("2. Department");
+                Console.WriteLine("3. Salary");
+                Console.WriteLine("4. All");
+                Console.Write("Enter choice: ");
+
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter new Name: ");
+                        found.Name = Console.ReadLine();
+
+                        break;
+
+                    case 2:
+                        Console.Write("Enter new Department: ");
+                        found.Department = Console.ReadLine();
+                        break;
+
+                    case 3:
+                        Console.Write("Enter new Salary: ");
+                        found.Salary = double.Parse(Console.ReadLine());
+                        break;
+
+                    case 4:
+                        Console.Write("Enter new Name: ");
+                        found.Name = Console.ReadLine();
+
+                        Console.Write("Enter new Department: ");
+                        found.Department = Console.ReadLine();
+
+                        Console.Write("Enter new Salary: ");
+                        found.Salary = double.Parse(Console.ReadLine());
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice!");
+                        return;
+                }
+                Employee.ViewEmployees();
+
+                Console.WriteLine("Employee Updated Successfully!");
+            }
+            else
+            {
+                Console.WriteLine("Employee not found.");
+            }
+        }
+        public static void DeleteEmployee()
+        {
+            Console.Write("Enter ID to delete: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            Employee found = null;
+
+            foreach (Employee emp in employees)
+            {
+                if (emp.Id == id)
+                {
+                    found = emp;
+                    break;
+                }
+            }
+
+            if (found != null)
+            {
+                employees.Remove(found);
+                Console.WriteLine("Employee Deleted Successfully!");
+                Employee.ViewEmployees();
+            }
+            else
+            {
+                Console.WriteLine("Employee not found.");
+                Employee.ViewEmployees();
+            }
+        }
+
+    }
+}
---
+++ Csharp/Assessment/FirstAssessmentCSharp/FirstAssessmentCSharp/SecondQuestionEmployee.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstAssessmentCSharp
+{
+    struct DateOfBirth
+    {
+        public int day;
+        public int month;
+        public int year;
+    }
+
+    struct Employ
+    {
+        public string name;
+        public DateOfBirth dob;
+    }
+    internal class SecondQuestionEmployee
+    {
+        public static void Run()
+        {
+            Console.Write("Enter number of employees: ");
+            int number_Of_Employee = Convert.ToInt32(Console.ReadLine());
+            Employ[] emp = new Employ[number_Of_Employee];
+
+            for (int i = 0; i < emp.Length; i++)
+            {
+                Console.WriteLine($"Enter details for Employee {i + 1}");
+
+                Console.Write("Name of the employee: ");
+                emp[i].name = Console.ReadLine();
+
+                Console.Write("Input day of the birth: ");
+                emp[i].dob.day = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Input month of the birth: ");
+                emp[i].dob.month = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Input year for the birth: ");
+                emp[i].dob.year = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n--- Employee Details ---");
+
+            foreach (Employ e in emp)
+            {
+                Console.WriteLine($"Name: {e.name}");
+                Console.WriteLine($"DOB: {e.dob.day}/{e.dob.month}/{e.dob.year}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
