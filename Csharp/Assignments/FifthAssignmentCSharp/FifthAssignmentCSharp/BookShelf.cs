48a8b7a060c12af05577132309336895ef39efb5
Giriraj Sivaraj <girirajs@infinite.com>
2026-04-06 01:03:56 +0530

    FifthAssignment

---
+++ Csharp/Assignments/FifthAssignmentCSharp/FifthAssignmentCSharp/BookShelf.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FifthAssignmentCSharp
+{
+
+
+    public class BookShelf
+    {
+        private Books[] bookArray = new Books[5]; // store 5 books
+
+        // Indexer
+        public Books this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < bookArray.Length)
+                    return bookArray[index];
+                else
+                    throw new IndexOutOfRangeException("Invalid Index");
+            }
+            set
+            {
+                if (index >= 0 && index < bookArray.Length)
+                    bookArray[index] = value;
+                else
+                    throw new IndexOutOfRangeException("Invalid Index");
+            }
+        }
+        public void DisplayAll()
+        {
+            foreach (Books book in bookArray)
+            {
+                if (book != null)
+                    book.Display();
+            }
+        }
+    }
+}
---
+++ Csharp/Assignments/FifthAssignmentCSharp/FifthAssignmentCSharp/Books.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FifthAssignmentCSharp
+{
+    public class Books
+    {
+        public string BookName { get; set; }
+        public string AuthorName { get; set; }
+
+        
+        public Books(string bookName, string authorName)
+        {
+            BookName = bookName;
+            AuthorName = authorName;
+        }
+
+        
+        public void Display()
+        {
+            Console.WriteLine("Book: " + BookName + ", Author: " + AuthorName);
+        }
+
+    }
+}
